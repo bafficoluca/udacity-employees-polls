fa60475d7af5c537a78b3bd168dc00c4aba89436
Luca Baffico <luca.baffico@evonove.it>
2022-07-05 17:14:02 +0200

    feat: snaphot test implemented for LoginPage in LoginPage.test.js

---
+++ src/pages/LoginPage.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { renderWithProviders } from "../utils/test-utils";
+import { MemoryRouter } from "react-router-dom";
+
+import LoginPage from "./LoginPage";
+
+describe("LoginPage", () => {
+  it("sould match the snapshot when no user is selected", () => {
+    var view = renderWithProviders(
+      <MemoryRouter initialEntries={["/"]}>
+        <LoginPage />
+      </MemoryRouter>
+    );
+    expect(view).toMatchSnapshot();
+  });
+});
